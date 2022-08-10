da8b3881926841a82b09d72f92ebf63c3783670f
Julian-Aquino <julian@xymilar.com>
2022-08-10 20:44:21 -0300

    Se agregan las animaciones

--- MercadoLiebre/routes/index.js
+++ MercadoLiebre/routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Home' });
 });
-router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
